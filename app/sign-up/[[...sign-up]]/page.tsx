e5f91d523a255cc0dda7542b2d90ce26874c10a6
Rithvik <rithvik.utd@outlook.com>
2024-12-21 20:28:49 -0500

    auth bug fixes

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <SignUp />
+      <SignUp afterSignUpUrl={"/SpendSense"} />
     </div>
   );
 }
