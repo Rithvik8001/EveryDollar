5a9cc39a3222c4c6fa705c923762cf7bbae6ab8f
Rithvik <rithvik.utd@outlook.com>
2024-12-24 22:43:06 -0500

    zustand and firebase integration fixes-6

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,15 +2,13 @@ import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
   if (!process.env.FIREBASE_PROJECT_ID) {
-    throw new Error("FIREBASE_PROJECT_ID is not set in environment variables");
+    throw new Error("FIREBASE_PROJECT_ID is not set");
   }
   if (!process.env.FIREBASE_CLIENT_EMAIL) {
-    throw new Error(
-      "FIREBASE_CLIENT_EMAIL is not set in environment variables"
-    );
+    throw new Error("FIREBASE_CLIENT_EMAIL is not set");
   }
   if (!process.env.FIREBASE_PRIVATE_KEY) {
-    throw new Error("FIREBASE_PRIVATE_KEY is not set in environment variables");
+    throw new Error("FIREBASE_PRIVATE_KEY is not set");
   }
 
   try {
